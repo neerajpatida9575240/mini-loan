f903529fb36b6bbfb90ff724d315a5314a3dc195
Neeraj Patidar <neeraj.patidar@arnasoftech.com>
2024-01-04 12:08:58 +0530

    footer

--- App/src/common/Footer.js
+++ App/src/common/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 const Footer = () => {
     return (
         <footer className="footer">
-          <div className="d-flex ">
+          <div className="footer-body">
             <p className="pe-4 mb-0">Terms and Conditions </p>  <p className="ps-4 mb-0"> Privacy Policy</p>
           </div>
         </footer>
